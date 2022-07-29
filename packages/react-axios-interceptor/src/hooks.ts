8821d733f23226b110fe3574b44e6f5c1993c0fc
hungtcs <hungtcs@outlook.com>
2022-07-29 23:43:46 +0800

    fix: 🐛 update useAxios check interceptors

--- packages/react-axios-interceptor/src/hooks.ts
+++ packages/react-axios-interceptor/src/hooks.ts
@@ -3,16 +3,18 @@ import { useContext, useMemo } from 'react';
 import axios, { AxiosRequestConfig } from 'axios';
 
 export function useAxios<D = any>(config?: AxiosRequestConfig<D>) {
-  const { interceptors = [] } = useContext(AxiosConfigContext);
+  const { interceptors } = useContext(AxiosConfigContext);
 
   const instance = useMemo(
     () => {
       const instance = axios.create(config);
 
-      for (const interceptor of interceptors) {
-        const { request, response } = interceptor;
-        instance.interceptors.request.use(request?.onFulfilled, request?.onRejected);
-        instance.interceptors.response.use(response?.onFulfilled, response?.onRejected);
+      if (interceptors) {
+        for (const interceptor of interceptors) {
+          const { request, response } = interceptor;
+          instance.interceptors.request.use(request?.onFulfilled, request?.onRejected);
+          instance.interceptors.response.use(response?.onFulfilled, response?.onRejected);
+        }
       }
 
       return instance;
